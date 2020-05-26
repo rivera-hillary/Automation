9fe4881f50bdaabe93265a718bbf3a32cd93198a
rivera-hillary <hrivera@wawand.co>
2020-05-26 11:54:37 -0500

    Third commit

--- cypress/integration/footerUrl.js
+++ cypress/integration/footerUrl.js
@@ -1,7 +1,6 @@
 describe('Dirección de url', function() {
     beforeEach('Ingresar a la página wawandco', function() {
         cy.visit('/');
-        cy.viewport(1000, 660);
     })
     it('Se dirija a la url Services', function() {
         cy.get('.links').contains('Services').click();
@@ -22,13 +21,26 @@ describe('Dirección de url', function() {
     it('Se dirija a la url Blog', function() {
         cy.get('.links').contains('Blog').click();
         cy.url().should('include','/blog');
-    })/*
+    })
     // Redes sociales
     it('Se dirija a la url de la red social Dribble', function() {
-        cy.get(':nth-child(2) > a > .fab').should('have.attr', 'href').and('include', 'dribble').then(href);
+        cy.get('.social > ul > li').eq(0).find('a').should('have.attr','href').and('eq','https://dribbble.com/wawandco');
     })
-    it('Se dirija a la url de la red social Dribble', function() {
-        cy.get('.social').should('have.attr','href').and('eq','https://dribbble.com/wawandco')
-        //cy.get(':nth-child(2) > a > .fab').should('have.attr','href').and('eq','https://dribbble.com/wawandco');
-    })*/
+    it('Se dirija a la url de la red social Facebook', function() {
+        cy.get('.social > ul > li').eq(1).find('a').should('have.attr','href').and('eq','https://www.facebook.com/wawandco/');
+    })
+    it('Se dirija a la url de la red social Github', function() {
+        cy.get('.social > ul > li').eq(2).find('a').should('have.attr','href').and('eq','https://github.com/wawandco');
+    })
+    it('Se dirija a la url de la red social Linkedin', function() {
+        cy.get('.social > ul > li').eq(3).find('a').should('have.attr','href').and('eq','https://www.linkedin.com/company/wawandco/');
+    })
+    it('Se dirija a la url de la red social Twitter', function() {
+        cy.get('.social > ul > li').eq(4).find('a').should('have.attr','href').and('eq','https://twitter.com/wawandco');
+    })
+    //Falla la prueba después de esperar 1 min y no dirigirse a la url
+    it('Se dirija a la url del correo', function() {
+        cy.get('.phone-mail > ul > li > a').click();
+        cy.url().should('include','/linkto:hello@wawand.co');
+    })
 })
--- cypress/integration/legaLink.js
+++ cypress/integration/legaLink.js
@@ -1,9 +1,11 @@
 describe('Confirmación del modal', function() {
     beforeEach('Ingresar a la página wawandco', function() {
         cy.visit('/');
-        cy.viewport(1000, 660);
     })
-    it('Comprobar que se muestre el modal', function() {
+    it('Comprobar contenga el link del modal', function() {
         cy.get('.iubenda-white').should('have.attr','href').and('eq','https://www.iubenda.com/privacy-policy/20396701');
+    })
+    it('Comprobar que esté el modal', function() {
+        cy.get('.iubenda-white').should('be.hidden').invoke('show').should('be.visible');
     })
 })
